f642767570ae097b73558717cd8f79fac87648e3
Jaiquez Hagood <west00brook@gmail.com>
2026-08-08 19:14:53 -0400

    dont want to track these files

--- database/queries/ALTER TABLE users.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE users
-ADD COLUMN notifications_read BOOLEAN DEFAULT FALSE;
--- database/queries/groupbyexample.sql
+++
@@ -1,7 +0,0 @@
-
-        SELECT 
-            post_id,
-            COUNT(*) AS likes
-        FROM posts_reactions
-        GROUP BY post_id
-      
